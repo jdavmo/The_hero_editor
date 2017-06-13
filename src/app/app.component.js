0319533e17e7fd7e5e44e3e3eebd8ce239e8645b
Alejandro Gomez A <alejandrogoa@gmail.com>
2017-06-13 23:11:25 +0100

    Tutorial routing terminado

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -15,7 +15,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n\t<h1>{{title}}</h1>\n\t<nav>\n\t\t <a routerLink=\"/dashboard\">Dashboard</a>\n\t\t <a routerLink=\"/heroes\">Heroes</a>\n\t</nav>\n\t<router-outlet></router-outlet>\n\n  "
+        template: "\n\t<h1>{{title}}</h1>\n\t<nav>\n\t\t <a routerLink=\"/dashboard\">Dashboard</a>\n\t\t <a routerLink=\"/heroes\">Heroes</a>\n\t</nav>\n\t<router-outlet></router-outlet>\n\n  ",
+        styles: ["\n    h1 {\n      font-size: 1.2em;\n      color: #999;\n      margin-bottom: 0;\n    }\n    h2 {\n      font-size: 2em;\n      margin-top: 0;\n      padding-top: 0;\n    }\n    nav a {\n      padding: 5px 10px;\n      text-decoration: none;\n      margin-top: 10px;\n      display: inline-block;\n      background-color: #eee;\n      border-radius: 4px;\n    }\n    nav a:visited, a:link {\n      color: #607D8B;\n    }\n    nav a:hover {\n      color: #039be5;\n      background-color: #CFD8DC;\n    }\n    nav a.active {\n      color: #039be5;\n    }\n  "]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
