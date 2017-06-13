845c138e998f1a40a8addd32b5481145bea124b7
Juan Narvaez <juan.narvaez@correounivall.edu.co>
2017-06-13 10:40:17 -0500

    Practica servicios con mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { HEROES } from './mock-heroes';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 @Component({
 	selector: 'my-app',
@@ -78,16 +69,27 @@ const HEROES: Hero[] = [
 	    margin-right: .8em;
 	    border-radius: 4px 0 0 4px;
 	  }
-	`]
+	`],
+	providers: [HeroService]
 })
 // export class AppComponent  { name = 'Angular'; }
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 	title = 'Tour of Heroes';
-  	heroes = HEROES;
+  	heroes = [];
   	selectedHero: Hero;
 
   	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+	}
+
+	constructor(private heroService: HeroService) { }
+
+	getHeroes(): void {
+		this.heroes = this.heroService.getHeroes();
+	}
+
+	ngOnInit(): void {
+		this.getHeroes();
 	}
 }
